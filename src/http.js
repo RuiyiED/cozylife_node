e98ca697091ea74735ce057bd6c8ce6e115e804a
Ader <Ader@gmail.com>
2020-03-18 16:06:07 +0800

    qqq

--- src/http.js
+++ src/http.js
@@ -3,7 +3,7 @@ const server = http.createServer((request, response)=>{
 response.writeHead(200, {
     'Content-Type': 'text/html'
 });
-response.end(`<h1>HelloXXX<h1><br>
+response.end(`<h1>HelloX<h1><br>
     ${request.url}
     `);
 });
